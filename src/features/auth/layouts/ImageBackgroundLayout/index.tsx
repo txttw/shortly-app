47a6bb6b43f8fefe47de19cd9c9991176c1146f9
Thomas Horvath <thorvath.ext@graphisoft.com>
2025-05-30 13:06:41 +0200

    css change on login layout

--- src/features/auth/layouts/ImageBackgroundLayout/index.tsx
+++ src/features/auth/layouts/ImageBackgroundLayout/index.tsx
@@ -1,11 +1,29 @@
 import { Outlet } from "react-router";
-import { Container } from "@mui/material";
+import { Container, useMediaQuery, useTheme } from "@mui/material";
 import "./css/layout.css";
 
 export default function ImageBackgroundLayout() {
+  const theme = useTheme();
+  const md_up = useMediaQuery(theme.breakpoints.up("md"));
   return (
     <div className="login-background-image">
-      <Container maxWidth="lg">
+      <Container
+        maxWidth="lg"
+        sx={
+          md_up
+            ? {
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "100%",
+              }
+            : {
+                width: "100%",
+                pt: 2,
+              }
+        }
+      >
         <Outlet />
       </Container>
     </div>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,32 +1,23 @@
-import { Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import LoginCard from "../features/auth/components/LoginCard";
 import ShareIcon from "@mui/icons-material/Share";
-import { useElementPosition } from "../hooks/useElementPosition";
 
 function LoginPage() {
-  const { boundingBox, windowDimensions } = useElementPosition(".login-card");
-  const theme = useTheme();
-  const md_up = useMediaQuery(theme.breakpoints.up("md"));
-
   return (
     <Grid
       container
       spacing={0}
       rowGap={{ xs: 4, md: 0 }}
-      style={{
-        padding: `${
-          md_up ? (windowDimensions.height - (boundingBox?.height || 0)) / 2 : 0
-        }px 0`,
-      }}
+      justifyContent={"center"}
     >
-      <Grid size={{ xs: 12, md: 7 }} alignSelf={"center"} color="#fff">
+      <Grid size={{ xs: 12, sm: 8, md: 7 }} alignSelf={"center"} color="#fff">
         <ShareIcon fontSize="large" sx={{ mb: 0.25, color: "#fff" }} />
         <Typography variant="h4" fontWeight={800}>
           Shortly
         </Typography>
         <Typography>Sign in or create an account</Typography>
       </Grid>
-      <Grid size={{ xs: 12, md: 5 }}>
+      <Grid size={{ xs: 12, sm: 8, md: 5 }}>
         <LoginCard />
       </Grid>
     </Grid>
